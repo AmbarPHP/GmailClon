026d5ce2a6a1f25caf5053d6ecc7599e18a32cba
AmbarPHP <perlamarina.franco@gmailcom>
2022-03-31 19:40:44 -0700

    adding initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
-import { AppMaterialModule } from './app.material.module';
+
 
 
 @NgModule({
@@ -13,7 +13,7 @@ import { AppMaterialModule } from './app.material.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,AppMaterialModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
